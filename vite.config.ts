6c56e16d9568a9061995f4cea85b6f28153b0053
jean <jean@ampyxpower.com>
2026-02-18 23:10:54 +0800

    Fix deployment config and base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for extension pages
+  base: '/aoraeth-web/', // Set base to repo name for GitHub Pages
   build: {
     outDir: 'dist',
     rollupOptions: {
